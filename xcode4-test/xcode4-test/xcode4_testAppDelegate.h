4885bceb9dbd8fa4418bad41caf284f5b64d30e5
danzell <dan.zell@live.com>
2011-03-28 14:03:19 -0400

    testing

---
+++ xcode4-test/xcode4-test/xcode4_testAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  xcode4_testAppDelegate.h
+//  xcode4-test
+//
+//  Created by Daniel Zell on 3/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface xcode4_testAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
